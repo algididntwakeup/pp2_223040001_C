322476738ce3084e7952698fc7b5ff1bcb66ce60
algididntwakeup <algisosmedacc@gmail.com>
2024-12-22 22:09:11 +0700

    pertemuan 12 submission

--- pertemuan12/MVC/src/Main.java
+++ pertemuan12/MVC/src/Main.java
@@ -1,15 +1,20 @@
-import controller.RegisterController;
+import model.MyBatisUtil;
 import model.UserMapper;
 import org.apache.ibatis.session.SqlSession;
-import view.RegisterView;
+
+import view.UserPdf;
+import view.UserView;
+import controller.UserController;
 
 public class Main {
     public static void main(String[] args) {
-        RegisterView view = new RegisterView();
         SqlSession session = MyBatisUtil.getSqlSession();
         UserMapper mapper = session.getMapper(UserMapper.class);
+        UserPdf pdf = new UserPdf();
 
-        new RegisterController(view, mapper);
+        UserView view = new UserView();
+        new UserController(view, mapper, pdf);
+
         view.setVisible(true);
     }
 }
--- tubes/src/model/UserMapper.java
+++ tubes/src/model/UserMapper.java
@@ -5,4 +5,6 @@ import org.apache.ibatis.annotations.Insert;
 public interface UserMapper {
     @Insert("INSERT INTO users (username, password, email) VALUES (#{username}, #{password}, #{email})")
     void registerUser(User user);
+    @Select("SELECT * FROM users WHERE username = #{username}")
+    User findUserByUsername(String username);
 }
